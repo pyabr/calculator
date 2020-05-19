27a90e2e7c1093eef6bd6843401b890cb250694f
mani <manijamali2003@gmail.com>
2020-05-19 21:22:37 +0430

    Commit all files

---
+++ control/postinstall.py
@@ -0,0 +1,12 @@
+from libnam import control
+
+## String Data Base ##
+control.write_record ("calculator.app_name","Calculator","/usr/share/locales/en.locale")
+control.write_record ("calculator.backspace","Backspace","/usr/share/locales/en.locale")
+control.write_record ("calculator.clear","Clear","/usr/share/locales/en.locale")
+control.write_record ("calculator.clear_all","Clear All","/usr/share/locales/en.locale")
+control.write_record ("calculator.mc","MC","/usr/share/locales/en.locale")
+control.write_record ("calculator.mr","MR","/usr/share/locales/en.locale")
+control.write_record ("calculator.ms","MS","/usr/share/locales/en.locale")
+control.write_record ("calculator.m+","M+","/usr/share/locales/en.locale")
+control.write_record ("calculator.sqrt","Sqrt","/usr/share/locales/en.locale")
