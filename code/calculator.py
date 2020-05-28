6d72a77f61128845cba88eea4346def455e3e042
mani <manijamali2003@gmail.com>
2020-05-28 18:52:16 +0430

    Commit all files

--- code/calculator.py
+++ code/calculator.py
@@ -119,7 +119,7 @@ class MainApp(QWidget):
         self.setLayout(mainLayout)
 
         self.Widget.setWindowTitle("Calculator")
-        self.Widget.setWindowIcon (res.get('@logo/calculator'))
+        self.Widget.setWindowIcon (QIcon(res.get('@logo/calculator')))
 
     def digitClicked(self):
         clickedButton = self.sender()
